b8e5b5e7a9531eaf0adbb0de91edd2a402dd770b
Nitesh Verma <niteshghn@gmail.com>
2023-05-13 22:41:57 +0530

    clean up

--- productsapi/models.py
+++ productsapi/models.py
@@ -4,8 +4,7 @@ from django.core.validators import MaxValueValidator,MinValueValidator
 # Create your models here.
 class Products(models.Model):
     name=models.CharField(max_length=200)
-    MRP=models.PositiveIntegerField()
-    ratings=models.IntegerField(validators=[MaxValueValidator(5),MinValueValidator(1)],default=1)
+    mrp=models.PositiveIntegerField()
     qty=models.PositiveIntegerField(verbose_name="quantity")
     image=models.URLField()
     in_stocks=models.BooleanField(default=True)
@@ -14,8 +13,9 @@ class Products(models.Model):
                    ('sports','sports'),
                    ('electronics','electronics'),
                    ('books','books'),
-                   ('HomeDecor','HomeDecor')]
+                   ('homedecor','homedecor')]
     types=models.CharField(max_length=20,choices=product_types)
+    brand = models.CharField(max_length=200)
 
     def __str__(self) :
         return self.name
