9de0cb7c7518fa9c72f2b882f615c057cfbbdaf4
Nitesh Verma <niteshghn@gmail.com>
2023-05-13 23:36:33 +0530

    fix product details

--- frontend/views.py
+++ frontend/views.py
@@ -8,6 +8,6 @@ def home(request):
 
 
 def product_details(request, id):
-    response = requests.get(f"http://localhost:8000/api/product/{id}")
+    response = requests.get(f"http://localhost:8000/api/products/{id}/")
     data = response.json()
     return render(request, "product_page.html", {"product": data})
